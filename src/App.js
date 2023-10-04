90ac710d7a5d4893548860d7b424658362e929b8
Aleksander Belomesytasev <lxwhlxwh@gmail.com>
2023-10-04 10:00:36 +0300

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="App">
       <>It works!</>
       <div>HAHA!</div>
-      <h3>it fuckn works!</h3>
+      <h3>it fuckn works! tiz</h3>
       <h2>Even from mac</h2>
     </div>
   );
